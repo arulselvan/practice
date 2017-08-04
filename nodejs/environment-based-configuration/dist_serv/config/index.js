a1a2870bf17244e95feecf7fd27225340e1d7206
Arul Selvan <arulselvanj@gmail.com>
2017-08-04 15:24:21 +0530

    added gitIgnore

--- nodejs/environment-based-configuration/dist_serv/config/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-//Todo Arul : added environment specific configuration, need to do some more research 
-var defaults = require("./default.json");
-var envConfig = require("./" + (process.env.NODE_ENV || "development") + ".json");
-exports.config = Object.assign({}, defaults, envConfig);
-//# sourceMappingURL=index.js.map
